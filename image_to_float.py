1cb5857ad775943f5e5a80019bd9c633c844859d
hubert <bercik999@gmail.com>
2016-05-29 14:24:41 +0200

    init

---
+++ image_to_float.py
@@ -0,0 +1,62 @@
+import glob, numpy, cv2
+
+TILE_SIZE = 25
+images = {}
+float_cache = {}
+CURRENT_TILE = 1
+current_tile_float = 1
+
+
+def warm_up():
+    for img in glob.glob(str(TILE_SIZE) + "/*.jpg"):
+        imread = cv2.imread(img)
+        images[img] = {'float' : imread.astype("float"), 'img': imread}
+
+
+def create_blank(width, height, rgb_color=(0, 0, 0)):
+    image = numpy.zeros((height, width, 3), numpy.uint8)
+    color = tuple(reversed(rgb_color))
+    image[:] = color
+    return image
+
+
+def mse(first_float, second):
+    err = numpy.sum((first_float - second['float']) ** 2)
+    err /= float(TILE_SIZE ** 2)
+    return err
+
+
+def find_similar(img):
+    compared_images = {}
+    floatval = img.astype("float")
+    for filename, tile_img in images.iteritems():
+        similarity = mse(floatval, tile_img)
+        compared_images[filename] = similarity
+    return min(compared_images, key=compared_images.get)
+
+warm_up()
+
+base_img = cv2.imread('base_img.jpg')
+height, width, channels = base_img.shape
+cropped_height = (height / TILE_SIZE) * TILE_SIZE
+cropped_width = (width / TILE_SIZE) * TILE_SIZE
+
+cropped_base_image = base_img[0:cropped_height, 0:cropped_width]
+rows = []
+image_from_tiles = create_blank(cropped_width, cropped_height)
+
+total_tiles = ((cropped_height / TILE_SIZE) + 1) * (cropped_width / TILE_SIZE)
+
+for x in range(1, (cropped_height / TILE_SIZE) + 1):
+    row = []
+    for y in range(1, (cropped_width / TILE_SIZE) + 1):
+        cropped_part = cropped_base_image[(x - 1) * TILE_SIZE: x * TILE_SIZE, (y - 1) * TILE_SIZE: y * TILE_SIZE]
+        similar_file = find_similar(cropped_part)
+        similar_fragment = cv2.imread(similar_file)
+        images.pop(similar_file)
+        image_from_tiles[(x - 1) * TILE_SIZE: x * TILE_SIZE, (y - 1) * TILE_SIZE: y * TILE_SIZE] = similar_fragment
+        cv2.imwrite('result.jpg', image_from_tiles)
+        print(str(y + (x-1) * ((cropped_width / TILE_SIZE) + 1)) + '/' + str(total_tiles))
+
+
+
